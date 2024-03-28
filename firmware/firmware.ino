01413d1278fbc17358b9060fdcd1829f021f411d
Carlos Peláez González <carlosprog1997@gmail.com>
2024-03-29 00:35:46 +0100

    Implemented new algorithm to read sensors

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -9,16 +9,74 @@
 
 #include <InfluxDbClient.h>
 #include <InfluxDbCloud.h>
-#include "EmonLib.h"   //https://github.com/openenergymonitor/EmonLib
 #include "config.h"
+
+
+#define V_SENSOR_PIN 35
+#define C_SENSOR_PIN 34
 
 
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
 Point house_power_sensor("house_power");
 
-EnergyMonitor emon;
-float kWh = 0;
-unsigned long lastmillis = millis();
+
+
+void read_power(float &voltage, float &current) {
+  uint16_t raw_voltage = 0;
+  uint16_t smoothed_raw_voltage = analogRead(V_SENSOR_PIN);
+  uint16_t min_smoothed_raw_voltage = 0xFFFF;
+  uint16_t max_smoothed_raw_voltage = 0;
+  float min_smoothed_voltage;
+  float max_smoothed_voltage;
+
+  uint16_t raw_current = 0;
+  float smoothed_raw_current = analogRead(C_SENSOR_PIN);
+  uint16_t min_smoothed_raw_current = 0xFFFF;
+  uint16_t max_smoothed_raw_current = 0;
+  float min_smoothed_current;
+  float max_smoothed_current;
+
+  float voltage_alpha = 0.5;
+  float current_alpha = 0.99;
+  uint32_t curr_millis = millis();
+  uint32_t prev_millis = millis();
+
+  while(curr_millis - prev_millis <= 5000) {
+
+    // Read raw values
+    raw_voltage = analogRead(V_SENSOR_PIN);
+    raw_current = analogRead(C_SENSOR_PIN);
+
+    // Smooth values
+    smoothed_raw_voltage = (1-voltage_alpha)*raw_voltage + voltage_alpha*smoothed_raw_voltage;
+    smoothed_raw_current = (1-current_alpha)*raw_current + current_alpha*smoothed_raw_current;
+
+    if(curr_millis - prev_millis >= 500) {
+      // Compute minimum/maximum
+      if(smoothed_raw_voltage < min_smoothed_raw_voltage)
+        min_smoothed_raw_voltage = smoothed_raw_voltage;
+      if(smoothed_raw_voltage > max_smoothed_raw_voltage)
+        max_smoothed_raw_voltage = smoothed_raw_voltage;
+  
+      if(smoothed_raw_current > max_smoothed_raw_current)
+        max_smoothed_raw_current = smoothed_raw_current;
+      if(smoothed_raw_current < min_smoothed_raw_current)
+        min_smoothed_raw_current = smoothed_raw_current;
+    }
+
+    curr_millis = millis();
+  }
+  min_smoothed_voltage = (min_smoothed_raw_voltage * 3.3f) / 4096;
+  max_smoothed_voltage = (max_smoothed_raw_voltage * 3.3f) / 4096;
+  min_smoothed_current = (min_smoothed_raw_current * 3.3f) / 4096;
+  max_smoothed_current = (max_smoothed_raw_current * 3.3f) / 4096;
+
+  voltage = (max_smoothed_voltage - min_smoothed_voltage) * 114.623;
+  current = (max_smoothed_current - min_smoothed_current) * 74.985 * 1.1339245735 - 0.71554617189;
+  if(current < 0)
+    current = 0;
+}
+
 
 
 void setup() {
@@ -49,20 +107,16 @@ void setup() {
     Serial.print("InfluxDB connection failed: ");
     Serial.println(client.getLastErrorMessage());
   }
-
-  // Sensor configuration
-  emon.voltage(35, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
-  emon.current(34, currCalibration); // Current: input pin, calibration.
 }
 
 void loop() {
   house_power_sensor.clearFields();
 
   // Read sensor values
-  emon.calcVI(20, 2000);
-  float voltage = emon.Vrms;
-  float intensity = emon.Irms;
-  float power = emon.apparentPower;
+  float voltage;
+  float intensity;
+  read_power(voltage, intensity);
+  float power = voltage * intensity;
 
   // Add values to InfluxDB point
   house_power_sensor.addField("voltage_V", voltage);
